8216398cb6ad6bbc9eb3466fde0aeecd6b3a275a
Nicholas Aelick <n.aelick@gmail.com>
2020-09-19 19:21:42 -0400

    Fix connection issues

--- flask-docker/app/main.py
+++ flask-docker/app/main.py
@@ -4,31 +4,29 @@ import time
 
 tries = 0
 while True:
-	try:
-		uri = "neo4j://neo4j:7687"
-		driver = GraphDatabase.driver(uri, auth=("neo4j", "test"))
-		break
-	except:
-		print("Failed to connect, retry in 5s")
-		time.sleep(5)
-		tries = tries + 1
-		if tries > 10:
-			break
+    try:
+        uri = "neo4j://neo4j:7687"
+        driver = GraphDatabase.driver(uri, auth=("neo4j", "test"))
+        break
+    except:
+        print("Failed to connect, retry in 5s")
+        time.sleep(5)
+        tries = tries + 1
+        if tries > 10:
+            break
 
 app = Flask(__name__)
 
+
 def bacon_number(tx):
-	query = (
-		'MATCH p=shortestPath('
-			'(bacon:Person {name:"Kevin Bacon"})-[*]-(meg:Person {name:"Meg Ryan"})'
-		')'
-		'RETURN p'
-	)
-	result = tx.run(query)
-	return result
+    query = ''
+    result = tx.run(query)
+    return result
+
 
 @app.route('/')
 def hello_world():
-	with driver.session() as session:
-		result = session.read_transaction(bacon_number)
-		return jsonify(result.data())
+    with driver.session() as session:
+        result = session.run(
+            'MATCH p=shortestPath((bacon:Person {name:"Kevin Bacon"})-[*]-(meg:Person {name:"Meg Ryan"})) RETURN p')
+        return jsonify(result.data())
