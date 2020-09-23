14a0be78f8129faa2b1b7a0f73481cd4f07b5536
Nicholas Aelick <naelick@espial.com>
2020-09-23 11:23:55 -0400

    Finalize for submission

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Replicant
 Short coding test for replicant
+
+#Setting up the data
+##For full ingest (Approx 12 hours):
+1. From the root folder run `docker-compose build` then `docker-compose up -d` to start the webserver and database
+2. From the ingest-docker folder run `docker build -t ingest .` `docker run -it -d --network replican_default ingest` to begin the ingest process. 
+
+ To save (some) time, you can download the movie dataset and put it in the ingest folder as `the-movies-dataset.zip`. It will be copied into the docker image. Otherwise the script will download it
+
+##Restore the database from neo4j.dump (Few minutes)
+ This procedure is more involved, but saves a considerable amount of time over injesting the data from scratch
+
+1. From the root folder run `docker-compose up -d --build` to start the webserver and database
+2. Wait for them to initialize, then shut them down
+3. Copy the neo4j.dump file from the root to `neo4j/data/`
+3. Run `docker-compose -f docker-compose-restore.yml up -d --build` to start a container for only the database, without having the database start
+4. Run `docker ps` and identify the id of the database container
+5. Run `docker exec -it <container id> bin/neo4j-admin load --from data/neo4j.dump --force`
+6. Shut down the restore container
+7. Run `docker-compose up -d` to restart the server
+
+#Using the api
+1. Go to `http://127.0.0.1/bacon_number?name=Tom Hanks`
+2. Replace the name in the url bar with other actor names
+
+
---
+++ flask/app/main.py
@@ -0,0 +1,65 @@
+from flask import Flask, jsonify, request, abort, g
+from neo4j import GraphDatabase
+import time
+import urllib.parse
+
+
+#----------Setup-------------------------------------
+app = Flask(__name__)
+def connect_to_database():
+    if not hasattr(g, 'neo4j_driver'):
+        tries = 0
+        while True:
+            try:
+                uri = "neo4j://neo4j:7687"
+                g.neo4j_driver = GraphDatabase.driver(uri, auth=("neo4j", "test"))
+                return g.neo4j_driver
+            except:
+                print("Failed to connect, retry in 5s")
+                time.sleep(5)
+                tries = tries + 1
+                if tries > 10:
+                    return None
+    else:
+        return g.neo4j_driver
+
+@app.teardown_appcontext
+def close_db(*args):
+    if hasattr(g, 'neo4j_driver'):
+        g.neo4j_driver.close()
+
+with app.app_context():
+    connect_to_database()
+
+#-----------Routes------------------------
+
+@app.route('/bacon_number')
+def bacon_number():
+    name = request.args.get("name")
+    name = urllib.parse.unquote(name)
+    if name == "Kevin Bacon":
+        #Neo4j doesn't like it when the path has 0 nodes
+        ret = {
+            "name": name,
+            "bacon_number": 0,
+            "path": []
+        }
+        return ret
+    driver = connect_to_database()
+    with driver.session() as session:
+        match_result = session.run("MATCH (n:Person {name: $name }) "
+                            "RETURN n", name=name)
+        person = match_result.single()
+        if person is None or person == []:
+            ret = {"name": name, "bacon_number": "Not available", "path": []}
+            return jsonify(ret)
+
+        result = session.run(
+            'MATCH p=shortestPath((bacon:Person {name:"Kevin Bacon"})-[*]-(meg:Person {name:$name})) RETURN p, length(p) as bacon_number', name = name)
+        result_data = result.data()
+        ret = {
+            "name": name,
+            "bacon_number": int(result_data[0]["bacon_number"] / 2),
+            "path": result_data[0]["p"]
+        }
+        return jsonify(ret)
